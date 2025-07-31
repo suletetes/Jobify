db91b40d0f968a02d9a8f5701382c87c148616aa
suleabdul <sulea841@gmail.com>
2025-07-31 22:24:59 +0100

    App

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import './App.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {
+    HomeLayout,
+    Landing,
+    Register,
+    Login,
+    DashboardLayout,
+    Stats,
+    Admin,
+    AllJobs,
+    EditJob,
+    Error,
+    AddJob,
+    Profile
+} from "./pages";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <HomeLayout/>,
+        children: [
+            {
+                index: true,
+                element: <Landing/>
+            },
+            {
+                path: 'register',
+                element: <Register/>
+            },
+            {
+                path: 'login',
+                element: <Login/>
+            },
+            {
+                path: 'dashboard',
+                element: <DashboardLayout/>
+            }
+
+        ]
+    },
+
+
+])
+
+
+function App() {
+
+    return <RouterProvider router={router}/>;
+
+}
+
+export default App
