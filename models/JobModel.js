9600245d5209143b688e9e7b1accc0fceec83ace
suleabdul <sulea841@gmail.com>
2025-08-03 14:37:25 +0100

    add constants to JobModel.js

--- models/JobModel.js
+++ models/JobModel.js
@@ -1,25 +1,25 @@
 import mongoose from 'mongoose';
-
+import { JOB_STATUS, JOB_TYPE } from '../utils/constants';
 const JobSchema = new mongoose.Schema(
-  {
-    company: String,
-    position: String,
-    jobStatus: {
-      type: String,
-      enum: ['interview', 'declined', 'pending'],
-      default: 'pending',
+    {
+        company: String,
+        position: String,
+        jobStatus: {
+            type: String,
+            enum: Object.values(JOB_STATUS),
+            default: JOB_STATUS.PENDING,
+        },
+        jobType: {
+            type: String,
+            enum: Object.values(JOB_TYPE),
+            default: JOB_TYPE.FULL_TIME,
+        },
+        jobLocation: {
+            type: String,
+            default: 'my city',
+        },
     },
-    jobType: {
-      type: String,
-      enum: ['full-time', 'part-time', 'internship'],
-      default: 'full-time',
-    },
-    jobLocation: {
-      type: String,
-      default: 'my city',
-    },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 export default mongoose.model('Job', JobSchema);
