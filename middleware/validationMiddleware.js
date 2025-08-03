8d3d04855e6b07e0daee45cbff90f57f4f2ea59e
suleabdul <sulea841@gmail.com>
2025-08-03 14:54:42 +0100

    updated withValidationErrors validateIdParam

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -11,6 +11,9 @@ const withValidationErrors = (validateValues) => {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
         const errorMessages = errors.array().map((error) => error.msg);
+        if (errorMessages[0].startsWith('no job')) {
+          throw new NotFoundError(errorMessages);
+        }
         throw new BadRequestError(errorMessages);
       }
       next();
@@ -41,7 +44,10 @@ export const validateJobInput = withValidationErrors([
 
 
 export const validateIdParam = withValidationErrors([
-  param('id')
-      .custom((value) => mongoose.Types.ObjectId.isValid(value))
-      .withMessage('invalid MongoDB id'),
+  param('id').custom(async (value) => {
+    const isValidId = mongoose.Types.ObjectId.isValid(value);
+    if (!isValidId) throw new BadRequestError('invalid MongoDB id');
+    const job = await Job.findById(value);
+    if (!job) throw new NotFoundError(`no job with id : ${value}`);
+  }),
 ]);
