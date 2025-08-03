fddb10981f9fea2ae1405a77164cc314419d5bf2
suleabdul <sulea841@gmail.com>
2025-08-03 20:46:24 +0100

    authController.js

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,8 +1,19 @@
-import { Router } from 'express';
-import { register, login } from '../controllers/authController.js';
+import {Router} from 'express';
+import {register, login} from '../controllers/authController.js';
+import {authorizePermissions} from '../middleware/authMiddleware.js';
+
+import {validateLoginInput} from '../middleware/validationMiddleware.js';
+
 const router = Router();
 
+
 router.post('/register', register);
-router.post('/login', login);
+router.post('/login', validateLoginInput, login);
+
+
+router.get('/admin/app-stats', [
+    authorizePermissions('admin'),
+    getApplicationStats,
+]);
 
 export default router;
