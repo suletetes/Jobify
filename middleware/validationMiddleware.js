33f560aa70b25112ef5c50bc4c88f88d661611c3
suleabdul <sulea841@gmail.com>
2025-08-03 15:16:27 +0100

    validateRegisterInput

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -1,54 +1,79 @@
-import { body, validationResult } from 'express-validator';
-import {BadRequestError, NotFoundError} from '../errors/customErrors';
-import { JOB_STATUS, JOB_TYPE } from '../utils/constants.js';
-import { param } from 'express-validator';
 import mongoose from "mongoose";
+import {body, validationResult, param} from 'express-validator';
+import {BadRequestError, NotFoundError} from '../errors/customErrors';
+import {JOB_STATUS, JOB_TYPE} from '../utils/constants.js';
+
+//
 import Job from "../client/src/assets/wrappers/Job.js";
+import User from '../models/UserModel.js';
+
 
 const withValidationErrors = (validateValues) => {
-  return [
-    validateValues,
-    (req, res, next) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        const errorMessages = errors.array().map((error) => error.msg);
-        if (errorMessages[0].startsWith('no job')) {
-          throw new NotFoundError(errorMessages);
-        }
-        throw new BadRequestError(errorMessages);
-      }
-      next();
-    },
-  ];
+    return [
+        validateValues,
+        (req, res, next) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                const errorMessages = errors.array().map((error) => error.msg);
+                if (errorMessages[0].startsWith('no job')) {
+                    throw new NotFoundError(errorMessages);
+                }
+                throw new BadRequestError(errorMessages);
+            }
+            next();
+        },
+    ];
 };
 
 export const validateTest = withValidationErrors([
-  body('name')
-    .notEmpty()
-    .withMessage('name is required')
-    .isLength({ min: 3, max: 50 })
-    .withMessage('name must be between 3 and 50 characters long')
-    .trim(),
+    body('name')
+        .notEmpty()
+        .withMessage('name is required')
+        .isLength({min: 3, max: 50})
+        .withMessage('name must be between 3 and 50 characters long')
+        .trim(),
 ]);
 
 
 export const validateJobInput = withValidationErrors([
-  body('company').notEmpty().withMessage('company is required'),
-  body('position').notEmpty().withMessage('position is required'),
-  body('jobLocation').notEmpty().withMessage('job location is required'),
-  body('jobStatus')
-      .isIn(Object.values(JOB_STATUS))
-      .withMessage('invalid status value'),
-  body('jobType').isIn(Object.values(JOB_TYPE)).withMessage('invalid job type'),
+    body('company').notEmpty().withMessage('company is required'),
+    body('position').notEmpty().withMessage('position is required'),
+    body('jobLocation').notEmpty().withMessage('job location is required'),
+    body('jobStatus')
+        .isIn(Object.values(JOB_STATUS))
+        .withMessage('invalid status value'),
+    body('jobType').isIn(Object.values(JOB_TYPE)).withMessage('invalid job type'),
 ]);
 
 
-
 export const validateIdParam = withValidationErrors([
-  param('id').custom(async (value) => {
-    const isValidId = mongoose.Types.ObjectId.isValid(value);
-    if (!isValidId) throw new BadRequestError('invalid MongoDB id');
-    const job = await Job.findById(value);
-    if (!job) throw new NotFoundError(`no job with id : ${value}`);
-  }),
+    param('id').custom(async (value) => {
+        const isValidId = mongoose.Types.ObjectId.isValid(value);
+        if (!isValidId) throw new BadRequestError('invalid MongoDB id');
+        const job = await Job.findById(value);
+        if (!job) throw new NotFoundError(`no job with id : ${value}`);
+    }),
+]);
+
+
+export const validateRegisterInput = withValidationErrors([
+    body('name').notEmpty().withMessage('name is required'),
+    body('email')
+        .notEmpty()
+        .withMessage('email is required')
+        .isEmail()
+        .withMessage('invalid email format')
+        .custom(async (email) => {
+            const user = await User.findOne({email});
+            if (user) {
+                throw new BadRequestError('email already exists');
+            }
+        }),
+    body('password')
+        .notEmpty()
+        .withMessage('password is required')
+        .isLength({min: 8})
+        .withMessage('password must be at least 8 characters long'),
+    body('location').notEmpty().withMessage('location is required'),
+    body('lastName').notEmpty().withMessage('last name is required'),
 ]);
