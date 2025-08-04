1d114c9e01b20d3eded75d90fbfef4d0480042e8
suleabdul <sulea841@gmail.com>
2025-08-04 16:18:18 +0100

    Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,27 +1,46 @@
-import {Link} from "react-router-dom";
-import Wrapper from "../assets/wrappers/RegisterAndLoginPage.js";
-import {FormRow, Logo} from "../components/index.js";
+import {Link, Form, redirect, useNavigation} from 'react-router-dom';
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+import {FormRow, Logo} from '../components';
+import customFetch from '../utils/customFetch';
+import {toast} from 'react-toastify';
+
+export const action = async ({request}) => {
+    const formData = await request.formData();
+    const data = Object.fromEntries(formData);
+    try {
+        await customFetch.post('/auth/login', data);
+        toast.success('Login successful');
+        return redirect('/dashboard');
+    } catch (error) {
+        toast.error(error?.response?.data?.msg);
+        return error;
+    }
+};
 
 const Login = () => {
+    const navigation = useNavigation();
+    const isSubmitting = navigation.state === 'submitting';
     return (
         <Wrapper>
-            <form  className="form">
+            <Form method='post' className='form'>
                 <Logo/>
-                <h1>Login Page</h1>
-                <FormRow type="email" name="email" defaultValue="john@gmail.com"/>
-                <FormRow type="password" name="password" defaultValue="secret123"/>
-                <button type="submit" className="btn btn-block">submit</button>
-                <button type="submit" className="btn btn-block">explore the app</button>
+                <h4>login</h4>
+                <FormRow type='email' name='email' defaultValue='john@gmail.com'/>
+                <FormRow type='password' name='password' defaultValue='secret123'/>
+                <button type='submit' className='btn btn-block' disabled={isSubmitting}>
+                    {isSubmitting ? 'submitting...' : 'submit'}
+                </button>
+                <button type='button' className='btn btn-block'>
+                    explore the app
+                </button>
                 <p>
                     Not a member yet?
-                    <Link to="/register" className="member-btn">
+                    <Link to='/register' className='member-btn'>
                         Register
                     </Link>
                 </p>
-            </form>
+            </Form>
         </Wrapper>
-
-)
-}
-
-export default Login
+    );
+};
+export default Login;
