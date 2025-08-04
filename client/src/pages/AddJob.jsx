be3f6f7e65b5b86eeef46efe27e55b8680e8ed88
suleabdul <sulea841@gmail.com>
2025-08-04 16:45:08 +0100

    add job status

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,7 +1,24 @@
 import {FormRow} from '../components';
 import Wrapper from '../assets/wrappers/DashboardFormPage';
-import {useOutletContext} from 'react-router-dom';
+import {redirect, useOutletContext} from 'react-router-dom';
 import {Form, useNavigation} from 'react-router-dom';
+import customFetch from "../utils/customFetch.js";
+import {JOB_TYPE} from "../../../utils/constants.js";
+
+
+export const action = async ({request}) => {
+    const formData = await request.formData();
+    const data = Object.fromEntries(formData);
+
+    try {
+        await customFetch.post('/jobs', data);
+        toast.success('Job added successfully');
+        return redirect('all-jobs');
+    } catch (error) {
+        toast.error(error?.response?.data?.msg);
+        return error;
+    }
+};
 
 const AddJob = () => {
     const {user} = useOutletContext();
@@ -21,6 +38,25 @@ const AddJob = () => {
                         name='jobLocation'
                         defaultValue={user.location}
                     />
+                    <div className='form-row'>
+                        <label htmlFor='jobStatus' className='form-label'>
+                            job status
+                        </label>
+                        <select
+                            name='jobStatus'
+                            id='jobStatus'
+                            className='form-select'
+                            defaultValue={JOB_TYPE.FULL_TIME}
+                        >
+                            {Object.values(JOB_TYPE).map((itemValue) => {
+                                return (
+                                    <option key={itemValue} value={itemValue}>
+                                        {itemValue}
+                                    </option>
+                                );
+                            })}
+                        </select>
+                    </div>
 
                     <button
                         type='submit'
