5844f69090119f62a23bb8af8bfcacf148cfc3a8
suleabdul <sulea841@gmail.com>
2025-08-04 16:25:56 +0100

    updated DashboardLayout.jsx

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,18 +1,37 @@
-import {Outlet} from "react-router-dom";
-import Wrapper from "../assets/wrappers/Dashboard.js";
-import {BigSidebar, Navbar, SmallSidebar} from "../components/index.js";
-import {createContext, useContext, useState} from "react";
-import {checkDefaultTheme} from "../App.jsx";
+import { Outlet, redirect, useNavigate, useNavigation } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/Dashboard';
+import { BigSidebar, Navbar, SmallSidebar, Loading } from '../components';
+import { createContext, useContext, useEffect, useState } from 'react';
+import customFetch from '../utils/customFetch';
+import { toast } from 'react-toastify';
+import { useQuery } from '@tanstack/react-query';
+import { checkDefaultTheme } from '../App';
+const userQuery = {
+    queryKey: ['user'],
+    queryFn: async () => {
+        const { data } = await customFetch.get('/users/current-user');
+        return data;
+    },
+};
 
+export const loader = (queryClient) => async () => {
+    try {
+        return await queryClient.ensureQueryData(userQuery);
+    } catch (error) {
+        return redirect('/');
+    }
+};
 
-const DashboardContext = createContext()
+const DashboardContext = createContext();
 
-const DashboardLayout = () => {
-    // temp
-    const user = {name: 'john'}
-    const [showSidebar, setShowSidebar] = useState(false)
-    const [isDarkTheme, setIsDarkTheme] = useState(checkDefaultTheme())
-
+const DashboardLayout = ({ queryClient }) => {
+    const { user } = useQuery(userQuery).data;
+    const navigate = useNavigate();
+    const navigation = useNavigation();
+    const isPageLoading = navigation.state === 'loading';
+    const [showSidebar, setShowSidebar] = useState(false);
+    const [isDarkTheme, setIsDarkTheme] = useState(checkDefaultTheme());
+    const [isAuthError, setIsAuthError] = useState(false);
 
     const toggleDarkTheme = () => {
         const newDarkTheme = !isDarkTheme;
@@ -22,32 +41,58 @@ const DashboardLayout = () => {
     };
 
     const toggleSidebar = () => {
-        setShowSidebar(!showSidebar)
-    }
+        setShowSidebar(!showSidebar);
+    };
+
     const logoutUser = async () => {
-        console.log("")
-    }
+        navigate('/');
+        await customFetch.get('/auth/logout');
+        queryClient.invalidateQueries();
+        toast.success('Logging out...');
+    };
+
+    customFetch.interceptors.response.use(
+        (response) => {
+            return response;
+        },
+        (error) => {
+            if (error?.response?.status === 401) {
+                setIsAuthError(true);
+            }
+            return Promise.reject(error);
+        }
+    );
+
+    useEffect(() => {
+        if (!isAuthError) return;
+        logoutUser();
+    }, [isAuthError]);
 
     return (
-        <DashboardContext.Provider value={{user, showSidebar, isDarkTheme, toggleDarkTheme, toggleSidebar, logoutUser}}>
-
+        <DashboardContext.Provider
+            value={{
+                user,
+                showSidebar,
+                isDarkTheme,
+                toggleDarkTheme,
+                toggleSidebar,
+                logoutUser,
+            }}
+        >
             <Wrapper>
-                <main className="dashboard">
-                    <SmallSidebar/>
-                    <BigSidebar/>
+                <main className='dashboard'>
+                    <SmallSidebar />
+                    <BigSidebar />
                     <div>
-                        <Navbar/>
-                        <div className="dashboard-page">
-                            <Outlet/>
+                        <Navbar />
+                        <div className='dashboard-page'>
+                            {isPageLoading ? <Loading /> : <Outlet context={{ user }} />}
                         </div>
                     </div>
                 </main>
-                <Outlet/>
             </Wrapper>
         </DashboardContext.Provider>
-
-    )
-}
-// eslint-disable-next-line react-refresh/only-export-components
-export const useDashboardContext = () => useContext(DashboardContext)
-export default DashboardLayout
+    );
+};
+export const useDashboardContext = () => useContext(DashboardContext);
+export default DashboardLayout;
