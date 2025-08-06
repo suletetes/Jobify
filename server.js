9561ca6c247090a8ee024678b87f198bb53844db
suleabdul <sulea841@gmail.com>
2025-08-06 14:37:35 +0100

    feat: integrate Cloudinary for image management and configure static file serving

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ import cookieParser from 'cookie-parser';
 import jobRouter from './routes/jobRouter.js';
 import authRouter from './routes/authRouter.js';
 import userRouter from "./routes/userRouter.js";
+import cloudinary from 'cloudinary';
 
 import {body, validationResult} from 'express-validator';
 
@@ -18,15 +19,31 @@ import {body, validationResult} from 'express-validator';
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
 import {authenticateUser} from './middleware/authMiddleware.js';
 
-dotenv.config();
-app.use(express.json());
-app.use(errorHandlerMiddleware);
-app.use(cookieParser());
+//
+import {dirname} from 'path';
+import {fileURLToPath} from 'url';
+import path from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.resolve(__dirname, './public')));
 
 
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET,
+});
+
+dotenv.config();
+app.use(express.json());
+app.use(errorHandlerMiddleware);
+app.use(cookieParser());
+
 
 const port = process.env.PORT || 5100;
 
@@ -46,7 +63,6 @@ app.get('/', (req, res) => {
 app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/auth', authRouter);
-
 
 
 app.post(
