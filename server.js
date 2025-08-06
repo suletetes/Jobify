40083e7d8f76a8b0897a897c29b3b02ed2bebfb4
suleabdul <sulea841@gmail.com>
2025-08-06 15:41:43 +0100

    feat: refactor server.js for improved structure and middleware integration

--- server.js
+++ server.js
@@ -1,37 +1,31 @@
-import express from 'express';
 import 'express-async-errors';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+import express from 'express';
 
 const app = express();
-import * as dotenv from 'dotenv';
-import morgan from "morgan";
+import morgan from 'morgan';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
+import cloudinary from 'cloudinary';
+import helmet from 'helmet';
+import mongoSanitize from 'express-mongo-sanitize';
 
-// routes
+// routers
 import jobRouter from './routes/jobRouter.js';
 import authRouter from './routes/authRouter.js';
-import userRouter from "./routes/userRouter.js";
-import cloudinary from 'cloudinary';
+import userRouter from './routes/userRouter.js';
 
-import {body, validationResult} from 'express-validator';
 
-// middleware
-import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
-import {authenticateUser} from './middleware/authMiddleware.js';
-
-//
+// public
 import {dirname} from 'path';
 import {fileURLToPath} from 'url';
 import path from 'path';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-app.use(express.static(path.resolve(__dirname, './public')));
-
-
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
-}
+// middleware
+import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
+import {authenticateUser} from './middleware/authMiddleware.js';
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
@@ -39,60 +33,46 @@ cloudinary.config({
     api_secret: process.env.CLOUD_API_SECRET,
 });
 
-dotenv.config();
-app.use(express.json());
-app.use(errorHandlerMiddleware);
-app.use(cookieParser());
-
-
-const port = process.env.PORT || 5100;
-
-try {
-    await mongoose.connect(process.env.MONGO_URL);
-    app.listen(port, () => {
-        console.log(`server running on PORT ${port}....`);
-    });
-} catch (error) {
-    console.log(error);
-    process.exit(1);
+const __dirname = dirname(fileURLToPath(import.meta.url));
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
 }
+app.use(express.static(path.resolve(__dirname, './client/dist')));
+app.use(cookieParser());
+app.use(express.json());
+app.use(helmet());
+app.use(mongoSanitize());
+
 app.get('/', (req, res) => {
     res.send('Hello World');
+});
+
+app.get('/api/v1/test', (req, res) => {
+    res.json({msg: 'test route'});
 });
 
 app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/auth', authRouter);
 
-
-app.post(
-    '/api/v1/test',
-    [body('name').notEmpty().withMessage('name is required')],
-    (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            const errorMessages = errors.array().map((error) => error.msg);
-            return res.status(400).json({errors: errorMessages});
-        }
-        next();
-    },
-    (req, res) => {
-        const {name} = req.body;
-        res.json({msg: `hello ${name}`});
-    }
-);
-
-app.use('*', (req, res) => {
-    res.status(404).send('Route not found')
-})
-
-app.listen(port, () => {
-    console.log('server running....');
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
 });
 
-/*
-* {
-* "company": "apple",
-* "position": "front-end"
-* }
-* */
+app.use('*', (req, res) => {
+    res.status(404).json({msg: 'not found'});
+});
+
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5100;
+
+try {
+    await mongoose.connect(process.env.MONGO_URL);
+    app.listen(port, () => {
+        console.log(`server running on PORT ${port}...`);
+    });
+} catch (error) {
+    console.log(error);
+    process.exit(1);
+}
