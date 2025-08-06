23bf5e50d8407d9ce22d8441fe075de694d0f30b
suleabdul <sulea841@gmail.com>
2025-08-06 15:07:03 +0100

    feat: enhance AllJobs component to include search parameters in context

--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -1,33 +1,42 @@
-import { toast } from 'react-toastify';
-import { JobsContainer, SearchContainer } from '../components';
+import {toast} from 'react-toastify';
+import {JobsContainer, SearchContainer} from '../components';
 import customFetch from '../utils/customFetch';
-import { useLoaderData } from 'react-router-dom';
-import { useContext, createContext } from 'react';
+import {useLoaderData} from 'react-router-dom';
+import {useContext, createContext} from 'react';
 
-export const loader = async ({ request }) => {
+const AllJobsContext = createContext();
+// eslint-disable-next-line react-refresh/only-export-components
+export const loader = async ({request}) => {
     try {
-        const { data } = await customFetch.get('/jobs');
+        const params = Object.fromEntries([
+            ...new URL(request.url).searchParams.entries(),
+        ]);
+
+        const {data} = await customFetch.get('/jobs', {
+            params,
+        });
+
         return {
             data,
+            searchValues: {...params},
         };
     } catch (error) {
-        toast.error(error?.response?.data?.msg);
+        toast.error(error.response.data.msg);
         return error;
     }
 };
 
-const AllJobsContext = createContext();
-
 const AllJobs = () => {
-    const { data } = useLoaderData();
+    const {data, searchValues} = useLoaderData();
 
     return (
-        <AllJobsContext.Provider value={{ data }}>
-            <SearchContainer />
-            <JobsContainer />
+        <AllJobsContext.Provider value={{data, searchValues}}>
+            <SearchContainer/>
+            <JobsContainer/>
         </AllJobsContext.Provider>
     );
 };
-
-export const useAllJobsContext = () => useContext(AllJobsContext);
 export default AllJobs;
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useAllJobsContext = () => useContext(AllJobsContext);
