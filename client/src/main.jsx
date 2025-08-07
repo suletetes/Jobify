08f1ee0ffd5db3cb035e360f76cc2a4137945314
suleabdul <sulea841@gmail.com>
2025-08-07 16:12:35 +0100

    add packages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,12 @@
-import React from 'react'
-import {createRoot} from 'react-dom/client'
-import App from './App.jsx'
-import customFetch from './utils/customFetch.js';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 import 'react-toastify/dist/ReactToastify.css';
-import {ToastContainer} from 'react-toastify';
-
-const data = await customFetch.get('/test');
-console.log(data);
-
-createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App/>
-        <ToastContainer position='top-center'/>
-    </React.StrictMode>
-)
-
+import './index.css';
+import { ToastContainer } from 'react-toastify';
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <>
+        <App />
+        <ToastContainer position='top-center' />
+    </>
+);
